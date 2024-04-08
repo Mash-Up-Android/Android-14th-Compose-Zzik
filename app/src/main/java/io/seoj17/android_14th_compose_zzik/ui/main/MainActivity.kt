3ae5dac87a1fe7de1135ad8105c2ba7899e07202
SEO-J17 <qw4643987@naver.com>
2024-04-09 00:00:22 +0900

    :sparkles: 바텀 네비게이션 추가

---
+++ app/src/main/java/io/seoj17/android_14th_compose_zzik/ui/main/MainActivity.kt
@@ -0,0 +1,35 @@
+package io.seoj17.android_14th_compose_zzik.ui.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import io.seoj17.android_14th_compose_zzik.model.CoinScreen
+import io.seoj17.android_14th_compose_zzik.navigation.CoinNavHost
+import io.seoj17.android_14th_compose_zzik.ui.bottom.BottomNavigation
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            
+            Scaffold(
+                bottomBar = {
+                    BottomNavigation(navHostController = navController)
+                }
+            ) { paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)) {
+                    CoinNavHost(
+                        startDestination = CoinScreen.Home,
+                        navHostController = navController,
+                    )
+                }
+            }
+        }
+    }
+}
