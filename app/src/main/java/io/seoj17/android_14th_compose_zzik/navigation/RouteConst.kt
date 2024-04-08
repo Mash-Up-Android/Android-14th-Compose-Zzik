87b6d075e55a03f8c34e635bd567540fb051be1b
sooziini <soojinpar1026@gmail.com>
2024-04-09 00:05:59 +0900

    feat: #1 경로 처리를 위한 NavHost 생성

---
+++ app/src/main/java/io/seoj17/android_14th_compose_zzik/navigation/RouteConst.kt
@@ -0,0 +1,7 @@
+package io.seoj17.android_14th_compose_zzik.navigation
+
+const val HOME_ROUTE = "home"
+const val COIN_INFO_ROUTE = "coin_info"
+const val INVESTMENT_DETAILS_ROUTE = "investment_details"
+const val DEPOSIT_WITHDRAWAL_ROUTE = "deposit_withdrawal"
+const val MORE_DETAILS_ROUTE = "more_details"
