6bc7b33a804a8bd3dc936334785159f1ffcb08b0
ze-zeh <chojh922@gmail.com>
2024-04-09 20:41:42 +0900

    #1 바텀 네비게이션 추가

---
+++ app/src/main/java/io/seoj17/android_14th_compose_zzik/BottomNav.kt
@@ -0,0 +1,157 @@
+package io.seoj17.android_14th_compose_zzik
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import io.seoj17.android_14th_compose_zzik.ui.home.account.Account
+import io.seoj17.android_14th_compose_zzik.ui.home.history.History
+import io.seoj17.android_14th_compose_zzik.ui.home.home.Exchange
+import io.seoj17.android_14th_compose_zzik.ui.home.info.Info
+import io.seoj17.android_14th_compose_zzik.ui.home.more.More
+import io.seoj17.android_14th_compose_zzik.ui.theme.Purple40
+import io.seoj17.android_14th_compose_zzik.ui.theme.PurpleGrey40
+
+@Composable
+fun BottomNav(
+    navController: NavHostController,
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        BottomNavTab(
+            Modifier.weight(1f),
+            BottomNavItem.Exchange,
+            currentRoute,
+            navController,
+        )
+
+        BottomNavTab(
+            Modifier.weight(1f),
+            BottomNavItem.Info,
+            currentRoute,
+            navController,
+        )
+
+        BottomNavTab(
+            Modifier.weight(1f),
+            BottomNavItem.History,
+            currentRoute,
+            navController,
+        )
+
+        BottomNavTab(
+            Modifier.weight(1f),
+            BottomNavItem.Account,
+            currentRoute,
+            navController,
+        )
+
+        BottomNavTab(
+            Modifier.weight(1f),
+            BottomNavItem.More,
+            currentRoute,
+            navController,
+        )
+    }
+}
+
+@Composable
+private fun BottomNavTab(
+    modifier: Modifier = Modifier,
+    item: BottomNavItem,
+    currentRoute: String?,
+    navController: NavHostController,
+) {
+    val selectedColor = if (currentRoute == item.route) Purple40 else PurpleGrey40
+
+    val onClick = {
+        navController.navigate(item.route) {
+            navController.graph.startDestinationRoute?.let {
+                popUpTo(it) { saveState = true }
+            }
+            launchSingleTop = true
+            restoreState = true
+        }
+    }
+
+    Column(
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .padding(vertical = 12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Icon(
+            painter = painterResource(id = item.icon),
+            contentDescription = stringResource(id = item.title),
+            modifier = Modifier.size(24.dp),
+            tint = selectedColor,
+        )
+
+        Text(
+            text = stringResource(id = item.title),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = selectedColor,
+            modifier = Modifier.padding(top = 6.dp),
+        )
+    }
+}
+
+@Composable
+fun BottomNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomNavItem.Exchange.route
+    ) {
+        composable(BottomNavItem.Exchange.route) {
+            Exchange()
+        }
+        composable(BottomNavItem.Info.route) {
+            Info()
+        }
+        composable(BottomNavItem.History.route) {
+            History()
+        }
+        composable(BottomNavItem.Account.route) {
+            Account()
+        }
+        composable(BottomNavItem.More.route) {
+            More()
+        }
+    }
+}
+
+sealed class BottomNavItem(
+    @StringRes val title: Int,
+    @DrawableRes val icon: Int,
+    val route: String,
+) {
+    data object Exchange : BottomNavItem(R.string.exchange, R.drawable.ic_exchange, "home/exchange")
+    data object Info : BottomNavItem(R.string.info, R.drawable.ic_info, "home/info")
+    data object History : BottomNavItem(R.string.history, R.drawable.ic_history, "home/history")
+    data object Account : BottomNavItem(R.string.account, R.drawable.ic_account, "home/account")
+    data object More : BottomNavItem(R.string.more, R.drawable.ic_more, "home/more")
+}
--- app/src/main/java/io/seoj17/android_14th_compose_zzik/ui/home/Home.kt
+++ app/src/main/java/io/seoj17/android_14th_compose_zzik/ui/home/Home.kt
@@ -1,11 +1,31 @@
 package io.seoj17.android_14th_compose_zzik.ui.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import io.seoj17.android_14th_compose_zzik.BottomNav
+import io.seoj17.android_14th_compose_zzik.BottomNavGraph
 
 @Composable
 fun Home() {
+    val navController = rememberNavController()
 
+    Scaffold(
+        bottomBar = {
+            if (navController.currentBackStackEntryAsState().value?.destination?.route?.startsWith("home") == true) {
+                BottomNav(navController = navController)
+            }
+        }
+    ) { paddingValues ->
+        Box(Modifier.padding(paddingValues)) {
+            BottomNavGraph(navController = navController)
+        }
+    }
 }
 
 @Preview
