51af0f8d00dcc2dbd1b30710402e88cd24718eb9
sooziini <soojinpar1026@gmail.com>
2024-04-10 16:33:51 +0900

    #1 NavigationBar 색상 및 label 세팅

--- app/src/main/java/io/seoj17/android_14th_compose_zzik/ui/common/BitBottomBar.kt
+++ app/src/main/java/io/seoj17/android_14th_compose_zzik/ui/common/BitBottomBar.kt
@@ -3,14 +3,20 @@ package io.seoj17.android_14th_compose_zzik.ui.common
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import io.seoj17.android_14th_compose_zzik.navigation.TopLevelDestination
+import io.seoj17.android_14th_compose_zzik.ui.theme.BitMainColor
+import io.seoj17.android_14th_compose_zzik.ui.theme.BitWhiteColor
 
 @Composable
 fun BitBottomBar(
@@ -19,8 +25,10 @@ fun BitBottomBar(
     modifier: Modifier = Modifier
 ) {
     val backStackEntry = navController.currentBackStackEntryAsState()
-
-    NavigationBar {
+    NavigationBar(
+        containerColor = BitMainColor,
+        modifier = modifier
+    ) {
         destinations.forEach { destination ->
             NavigationBarItem(
                 selected = destination.route == backStackEntry.value?.destination?.route,
@@ -36,9 +44,19 @@ fun BitBottomBar(
                 icon = {
                     Icon(
                         painter = painterResource(id = destination.iconId),
-                        contentDescription = destination.text
+                        contentDescription = destination.text,
+                        tint = BitWhiteColor
                     )
-                }
+                },
+                label = {
+                    Text(
+                        text = destination.text,
+                        color = BitWhiteColor
+                    )
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = BitMainColor
+                )
             )
         }
     }
