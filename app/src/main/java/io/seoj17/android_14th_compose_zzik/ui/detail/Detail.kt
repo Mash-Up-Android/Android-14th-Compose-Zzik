ef9ff3cffbe77dfa3f35882ec0cf50c81490ff2b
ze-zeh <chojh922@gmail.com>
2024-04-16 20:49:30 +0900

    #1 Detail 뷰 추가

---
+++ app/src/main/java/io/seoj17/android_14th_compose_zzik/ui/detail/Detail.kt
@@ -0,0 +1,29 @@
+package io.seoj17.android_14th_compose_zzik.ui.detail
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.seoj17.android_14th_compose_zzik.ui.component.UpBitTopBar
+import io.seoj17.android_14th_compose_zzik.ui.theme.Android14thComposeZzikTheme
+
+@Composable
+fun Detail(title: String) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        UpBitTopBar(
+            title = title,
+            onBackButtonClick = { },
+        )
+    }
+}
+
+@Composable
+@Preview
+fun DetailPreview() {
+    Android14thComposeZzikTheme {
+        Detail("도지코인(DOGE/KRW)")
+    }
+}
