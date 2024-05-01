dc6718a3dae890ff4a34082d1a2dc6201cd9f20d
sooziini <soojinpar1026@gmail.com>
2024-05-01 19:40:59 +0900

    feat: #3 detail 화면에서 bottom bar 숨김 처리

--- app/src/main/java/io/seoj17/android_14th_compose_zzik/BitApp.kt
+++ app/src/main/java/io/seoj17/android_14th_compose_zzik/BitApp.kt
@@ -3,9 +3,15 @@ package io.seoj17.android_14th_compose_zzik
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import io.seoj17.android_14th_compose_zzik.navigation.BitNavHost
+import io.seoj17.android_14th_compose_zzik.navigation.BitNavigationRoute
 import io.seoj17.android_14th_compose_zzik.navigation.TopLevelDestination
 import io.seoj17.android_14th_compose_zzik.ui.common.BitBottomBar
 import io.seoj17.android_14th_compose_zzik.ui.theme.BitBackgroundColor
@@ -14,12 +20,20 @@ import io.seoj17.android_14th_compose_zzik.ui.theme.BitWhiteColor
 @Composable
 fun BitApp() {
     val navController = rememberNavController()
+    var showBottomBar by rememberSaveable { mutableStateOf(true) }
+    showBottomBar = when (navController.currentBackStackEntryAsState().value?.destination?.route) {
+        BitNavigationRoute.HomeGraph.DetailRoute.route -> false
+        else -> true
+    }
+
     Scaffold(
         bottomBar = {
-            BitBottomBar(
-                destinations = TopLevelDestination.entries,
-                navController = navController
-            )
+            if (showBottomBar) {
+                BitBottomBar(
+                    destinations = TopLevelDestination.entries,
+                    navController = navController
+                )
+            }
         },
         containerColor = BitBackgroundColor,
         contentColor = BitWhiteColor
